396ef445d09f03c227ab4b4c597b261b1d60b0d3
Colin Knebl <colin.knebl@outlook.com>
2018-11-18 07:24:30 -0500

    Apollo client set up

---
+++ sick-fits/frontend/components/CreateItem.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Router from 'next/router';
+import Error from './ErrorMessage';
+import Form from './styles/Form';
+import fromatMoney from '../lib/formatMoney';
+
+const CREATE_ITEM_MUTATION = gql`
+	mutation CREATE_ITEM_MUTATION(
+		$title: String!
+		$description: String!
+		$price: Int!
+		$image: String
+		$largeImage: String
+	) {
+		createItem(
+			title: $title
+			description: $description
+			price: $price
+			image: $image
+			largeImage: $largeImage
+		) {
+			id
+		}
+	}
+`;
+
+class CreateItem extends Component {
+	state = {
+		title: '',
+		description: '',
+		price: 0,
+		image: '',
+		largeImage: ''
+	};
+	render() {
+		return (
+			<Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+				{(createItem, { loading, error }) => (
+					<Form
+						onSubmit={async event => {
+							event.preventDefault();
+							const res = await createItem(this.state);
+							console.log('res', res);
+							Router.push({
+								pathname: '/item',
+								query: { id: res.data.createItem.id }
+							});
+						}}
+					>
+						<Error error={error} />
+						<fieldset disabled={loading} aria-busy={loading}>
+							<label htmlFor="file">
+								Image
+								<input
+									type="file"
+									id="file"
+									name="file"
+									placaholder="Upload an Image"
+									required
+									onChange={this.uploadFile}
+								/>
+								{this.state.image && (
+									<img
+										src={this.state.image}
+										alt="Upload Preview"
+										style={{ width: '200px' }}
+									/>
+								)}
+							</label>
+
+							<label htmlFor="title">
+								Title
+								<input
+									type="text"
+									id="title"
+									name="title"
+									placaholder="Title"
+									required
+									value={this.state.title}
+									onChange={this.handleChange}
+								/>
+							</label>
+
+							<label htmlFor="price">
+								Price
+								<input
+									type="number"
+									id="price"
+									name="price"
+									placaholder="Price"
+									required
+									value={this.state.price}
+									onChange={this.handleChange}
+								/>
+							</label>
+
+							<label htmlFor="description">
+								Description
+								<textarea
+									type="text"
+									id="description"
+									name="description"
+									placaholder="Description"
+									required
+									value={this.state.description}
+									onChange={this.handleChange}
+								/>
+							</label>
+
+							<button type="submit">Submit</button>
+						</fieldset>
+					</Form>
+				)}
+			</Mutation>
+		);
+	}
+	handleChange = event => {
+		const { name, type, value } = event.target;
+		const val = type === 'number' ? parseFloat(value) : value;
+		this.setState({ [name]: val });
+	};
+	uploadFile = async event => {
+		const files = event.target.files;
+		const data = new FormData();
+		data.append('file', files[0]);
+		data.append('upload_preset', 'sickfits');
+		const options = {
+			method: 'POST',
+			body: data
+		};
+		const res = await fetch(
+			'https://api.cloudinary.com/v1_1/colinknebl/image/upload',
+			options
+		);
+		const file = await res.json();
+
+		this.setState({
+			image: file.secure_url,
+			largeImage: file.eager[0].secure_url
+		});
+	};
+}
+
+export default CreateItem;
+export { CREATE_ITEM_MUTATION };
---
+++ sick-fits/frontend/setUpApolloClient.md
@@ -0,0 +1,46 @@
+# withData.js
+
+## Imports
+
+1. bring in ApolloClient from 'apollo-boost' (npm package)
+2. bring in withApollo from 'next-with-apollo' (npm package)
+
+# \_app.js (create Apollo client)
+
+## Imports
+
+1. bring in ApolloProvider from 'react-apollo'
+2. bring in withData from '../lib/withData'
+
+## Implement
+
+1. wrap app with the ApolloProvider
+2. ApolloProvider needs a client
+3. write getItitialProps func (this is a next.js function, not react. because next.js is server rendered, this method is needed)
+4. pass page props to rendered Component
+
+# To set up a query from a page or component (see components/Items.js for example)
+
+## import
+
+1. import { Query } from 'react-apollo';
+2. import gql from 'graphql-tag';
+
+## Other steps
+
+1. write the query outside of the class
+2. add Query component (from react-apollo), and pass it the query (e.x. query={ALL_ITEMS_QUERY} )
+3. NOTE: the only child of a Query component can be a function with a single argument - 'payload' - that includes the fetched data, any error, and if loading boolean (as well as other things)
+
+# To set up a mutation from a page or component (see components/CreateItem.js for example)
+
+## import
+
+1. import { Mutation } from 'react-apollo';
+2. import gql from 'graphql-tag';
+
+## Other steps
+
+1. write mutation outside of the class
+2. add Mutation component to jsx, passing it the mutation and variables (e.x. mutation={CREATE_ITEM_MUTATION} variables={this.state})
+3. NOTE: the only child of a Mutation component can be a function with 2 arguments, the mutation and a 'payload. the payload includes loading and error (as well as other things)
